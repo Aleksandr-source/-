4243acb17a9462d2b752620d35636d6587409268
Legion <kovalenkoaleksandr132@gmail.com>
2025-10-16 02:10:47 +0500

    feat: Initial Android project structure (Manual setup for Gradle 8.0.2)

---
+++ app/src/main/kotlin/AI/summarization/program/MainActivity.kt
@@ -0,0 +1,11 @@
+package AI.summarization.program
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
